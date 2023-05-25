1b086db5d122efa04eb2eae7828116a815549580
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-05-25 16:00:57 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn=document.querySelector('[data-start]');
+const stopBtn=document.querySelector('[data-stop]');
+const bodyEl=document.querySelector('body');
+console.log(startBtn);
 
+startBtn.addEventListener('click', onShowStart);
+stopBtn.addEventListener('click', onShowStop);
+stopBtn.setAttribute('disabled','');
+let timerId=null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onShowStart(){
+    startBtn.setAttribute('disabled','');
+    stopBtn.removeAttribute('disabled','');
+    timerId=setInterval(()=>{
+    let newColor=getRandomHexColor();
+    bodyEl.style.backgroundColor=newColor;
+    
+    },1000);
+
+}
+
+function onShowStop(){
+startBtn.removeAttribute('disabled','');
+stopBtn.setAttribute('disabled','');
+clearInterval(timerId);
+}
